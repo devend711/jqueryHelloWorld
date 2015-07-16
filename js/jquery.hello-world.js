ee508a3ca628ae096d1e593d2390f01848ef9b29
Deven <devend711@gmail.com>
2015-07-15 22:36:31 -0400

    callbacks

--- js/jquery.hello-world.js
+++ js/jquery.hello-world.js
@@ -1,8 +1,23 @@
 (function($) {
-  $.fn.helloWorld = function() {
+  $.fn.helloWorld = function(opts) {
+
+    var settings = $.extend({
+      text: 'Hello, world',
+      color: null,
+      callback: null
+    }, opts); // merge any opts that were passed
+
     // 'this' is already a jQuery object
     return this.each( function() {
-      $(this).text("Hello, World!");
+      $(this).text(settings.text);
+
+      if (settings.color) {
+        $(this).css('color', settings.color);
+      }
+
+      if ($.isFunction(settings.callback)){
+        settings.callback.call(this); // call the callback function on this jQuery object
+      }
     });
   }
 }(jQuery));
