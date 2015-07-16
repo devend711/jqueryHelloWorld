19632aeeef443b5d992379fc0f1f3842d6b69926
Deven <devend711@gmail.com>
2015-07-15 22:26:32 -0400

    it works

--- js/jquery.hello-world.js
+++ js/jquery.hello-world.js
@@ -1,9 +1,8 @@
 (function($) {
-
-    $.fn.helloWorld = function() {
-
-        // Future home of "Hello, World!"
-
-    }
-
+  $.fn.helloWorld = function() {
+    // 'this' is already a jQuery object
+    this.each( function() {
+      $(this).text("Hello, World!");
+    });
+  }
 }(jQuery));
